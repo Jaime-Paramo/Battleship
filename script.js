50b3e2fc5d114a4a095d2ed006aef18d076e7031
Your Name <jaime.a.paramo1@gmail.com>
2023-05-15 08:57:31 -0400

    grabbed everything from ver.3 to ver.1

--- script.js
+++ script.js
@@ -1,263 +1,228 @@
-document.addEventListener('DOMContentLoaded', () => {
-      const playerBoard = document.getElementById('player-board');
-      const computerBoard = document.getElementById('computer-board');
-      const startButton = document.getElementById('start-button');
-      const rotateButton = document.getElementById('flip-button');
-      const turnDisplay = document.getElementById('turn-display');
-      const infoDisplay = document.getElementById('info');
-      const userSquares = [];
-      const computerSquares = [];
-      let isHorizontal = true;
-      let isGameOver = false;
-      let currentPlayer = 'user';
-      const width = 10;
-      let ready = false;
-      let enemyReady = false;
-      let allShipsPlaced = false;
-      let shotFired = -1;
-    
-      createBoard(playerBoard);
-      createBoard(computerBoard);
-    
-      // Generate game boards
-      function createBoard(board) {
-        for (let x = 0; x < width; x++) {
-          for (let y = 0; y < width; y++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.setAttribute('data-x', x);
-            cell.setAttribute('data-y', y);
-            board.appendChild(cell);
+const gamesBoardContainer = document.querySelector("#gamesboard-container");
+const optionContainer = document.querySelector(".option-container");
+const flipButton = document.querySelector("#flip-button");
+
+//Game state. Use this object to keep track of where the ships are on each board
+let state = {
+      playerBoard: {
+        ships: {
+          destroyer: {
+            shipType: 'destroyer',
+            length: 2,
+            coordinates: []  // example: [0, 1]
+          },
+          submarine: {
+            shipType: 'submarine',
+            length: 3,
+            coordinates: []  // example: [2, 3, 4]
+          },
+          cruiser: {
+            shipType: 'cruiser',
+            length: 3,
+            coordinates: []  // example: [5, 6, 7]
+          },
+          battleship: {
+            shipType: 'battleship',
+            length: 4,
+            coordinates: []  // example: [8, 9, 10, 11]
+          },
+          carrier: {
+            shipType: 'carrier',
+            length: 5,
+            coordinates: []  // example: [12, 13, 14, 15, 16]
+          }
+        }
+      },
+      //grabbing destroyer coordinates: state.computerBoard.ships.destroyer.coordinates
+      computerBoard: { 
+        ships: {
+          destroyer: {
+            shipType: 'destroyer',
+            length: 2,
+            coordinates: []  // example: [17, 18]
+          },
+          submarine: {
+            shipType: 'submarine',
+            length: 3,
+            coordinates: []  // example: [19, 20, 21]
+          },
+          cruiser: {
+            shipType: 'cruiser',
+            length: 3,
+            coordinates: []  // example: [22, 23, 24]
+          },
+          battleship: {
+            shipType: 'battleship',
+            length: 4,
+            coordinates: []  // example: [25, 26, 27, 28]
+          },
+          carrier: {
+            shipType: 'carrier',
+            length: 5,
+            coordinates: []  // example: [29, 30, 31, 32, 33]
           }
         }
       }
+    }
     
-      // Rotate the ships
-      rotateButton.addEventListener('click', () => {
-        isHorizontal = !isHorizontal;
-        toggleShipOrientation();
-      });
-    
-      function toggleShipOrientation() {
-        const ships = document.querySelectorAll('.option');
-        ships.forEach((ship) => {
-          ship.classList.toggle('vertical');
-        });
-      }
-    
-      // Place ships on the board
-      const options = document.querySelectorAll('.option');
-    
-      options.forEach((option) => {
-        option.addEventListener('dragstart', dragStart);
-        option.addEventListener('dragover', dragOver);
-        option.addEventListener('dragenter', dragEnter);
-        option.addEventListener('dragleave', dragLeave);
-        option.addEventListener('drop', dragDrop);
-        option.addEventListener('dragend', dragEnd);
-      });
-    
-      let selectedShipNameWithIndex = '';
-      let draggedShip = null;
-      let draggedShipLength = 0;
-    
-      function dragStart() {
-        selectedShipNameWithIndex = this.dataset.ship;
-        draggedShip = this;
-        draggedShipLength = this.childNodes.length;
-      }
-    
-      function dragOver(e) {
-        e.preventDefault();
-      }
-    
-      function dragEnter(e) {
-        e.preventDefault();
-      }
-    
-      function dragLeave() {
-        // console.log('drag leave');
-      }
-    
-      function dragDrop() {
-            const shipNameWithLastId = draggedShip.lastChild.id;
-            const shipClass = shipNameWithLastId.slice(0, -2);
-            const lastShipIndex = parseInt(shipNameWithLastId.substr(-1));
-            let shipLastId = lastShipIndex + parseInt(this.dataset.x);
-          
-            if (isHorizontal) {
-              for (let i = 0; i < draggedShipLength; i++) {
-                const directionClass = getDirectionClass(i, draggedShipLength);
-                userSquares[parseInt(this.dataset.x) + i + parseInt(this.dataset.y) * width].classList.add(
-                  'taken',
-                  'horizontal',
-                  directionClass,
-                  shipClass
-                );
-                userSquares[parseInt(this.dataset.x) + i + parseInt(this.dataset.y) * width].setAttribute(
-                  'data-ship',
-                  shipNameWithLastId
-                );
-              }
-            } else {
-              for (let i = 0; i < draggedShipLength; i++) {
-                const directionClass = getDirectionClass(i, draggedShipLength);
-                userSquares[
-                  parseInt(this.dataset.x) + parseInt(this.dataset.y) * width + i * width
-                ].classList.add('taken', 'vertical', directionClass, shipClass);
-                userSquares[
-                  parseInt(this.dataset.x) + parseInt(this.dataset.y) * width + i * width
-                ].setAttribute('data-ship', shipNameWithLastId);
-              }
-            }
-          
-            // Check if all ships are placed
-            const placedShips = document.querySelectorAll('.option.taken');
-            if (placedShips.length === totalNumberOfShips) {
-              allShipsPlaced = true;
-              // Perform additional actions when all ships are placed
-              startButton.disabled = false; // Enable the start button when all ships are placed
-              infoDisplay.innerHTML = 'All ships are placed. Click "Start" to begin the game.';
-            }
-          }
 
-      function dragEnd() {
-            // Reset variables and styles
-            selectedShipNameWithIndex = '';
-            draggedShip = null;
-            draggedShipLength = 0;
-          
-            // Add a delay before allowing the computer to start its turn
-            setTimeout(() => {
-              if (!isGameOver) {
-                if (currentPlayer === 'user') {
-                  // Call a function to start the computer's turn
-                  startComputerTurn();
-                } else {
-                  // Call a function to start the user's turn
-                  startUserTurn();
-                }
-              }
-            }, 1000);
-          }
-          //fix the ternery
-      function getDirectionClass(i, draggedShipLength) {
-            return i === 0 ? 'start': i === draggedShipLength;
-      }
-
-        //Start the game 
-      startButton.addEventListener('click', startGame);
-      if (allShipsPlaced) {
-            startButton.disabled = true; // Disable the start button once the game starts
-            rotateButton.disabled = true; // Disable the rotate button once the game starts
-            turnDisplay.innerHTML = 'Your Turn'; // Display the initial turn as the user's turn
-        
-            // Add event listeners for firing shots
-            computerSquares.forEach((square) => {
-              square.addEventListener('click', () => {
-                if (currentPlayer === 'user' && !isGameOver && square.classList.contains('cell')) {
-                  processUserShot(square);
-                }
-              });
-            });
-        
-            generateComputerShips();
-        
-            startComputerTurn();
+// Flip the ships
+let angle = 0;
+function flip() {
+      const optionShips = Array.from(optionContainer.children);
+      angle = angle === 0 ? 90 : 0;
+      optionShips.forEach(optionShip => {
+          optionShip.style.transform = `rotate(${angle}deg)`;
+          if (angle === 90) {
+              optionShip.classList.add("rotate90");
           } else {
-            infoDisplay.innerHTML = 'Please place all your ships on the board.';
+              optionShip.classList.remove("rotate90");
           }
-        });
-
-        // Event listener for firing shots
-      computerSquares.forEach((square) => {
-            square.addEventListener('click', () => {
-            if (currentPlayer === 'user' && !isGameOver && square.classList.contains('cell')) {
-            processUserShot(square);
-            }
-            });
       });
-      
-      // Function to handle user's shot
-      function processUserShot(square) {
-            if (square.classList.contains('hit') || square.classList.contains('miss')) {
-            return; // Ignore if the square has already been shot
-            }
-      
-            const cell = square.dataset.x + square.dataset.y;
-            const isHit = // Add your logic to determine if it's a hit or a miss
-      
-            // Add classes to the square based on the result of the shot
-            square.classList.add(isHit ? 'hit' : 'miss');
-      
-            // Check if the game is over (e.g., all ships are sunk)
-            const isUserWin = computerSquares.every((squares) => 
-            square.classList.contains('taken') && square.classList.contains('hit')
-            );
-            const isComputerWin = userSquares.every((square) =>
-            square.classList.contains('taken') && square.classList.contains('hit')
-            );
-      
-            if (isUserWin || isComputerWin) {
-            endGame(isUserWin ? 'user' : 'computer');
-            } else {
-            // Switch turns between user and computer
-            currentPlayer = 'computer';
-      
-            // Update turn display
-            turnDisplay.innerHTML = "Computer's Turn";
-      
-            // Call function to process the computer's shot after a delay
-            setTimeout(processComputerShot, 1000);
-            }
+  }
+  
+
+  let isHorizontal = true;
+
+  flipButton.addEventListener("click", () => {
+      isHorizontal = !isHorizontal;
+      flip();
+  });
+  
+
+flipButton.addEventListener("click", flip);
+
+// Creating Boards
+const width = 10;
+
+function createBoard(color, user) {
+      const gameBoardContainer = document.createElement('div')
+      gameBoardContainer.classList.add('game-board')
+      gameBoardContainer.style.backgroundColor = color
+      gameBoardContainer.id = user
+
+      for (let i = 0; i < width * width; i++) {
+          const block = document.createElement('div')
+          block.classList.add('block')
+          block.id = i
+          gameBoardContainer.append(block)
       }
-      
-      // Function to handle computer's shot
-      function processComputerShot() {
-            // Add your logic to determine the square to fire at
-            const randomIndex = Math.floor(Math.random() * computerSquares.length);
-            const square = computerSquares[randomIndex];
-            
-            if (square.classList.contains('hit') || square.classList.contains('miss')) {
-            processComputerShot(); // Retry if the square has already been shot
-            return;
-            }
-      
-            const cell = square.dataset.x + square.dataset.y;
-            const isHit = // Add your logic to determine if it's a hit or a miss
-      
-            // Add classes to the square based on the result of the shot
-            square.classList.add(isHit ? 'hit' : 'miss');
-      
-            // Check if the game is over (e.g., all ships are sunk)
-            const isUserWin = // Add your logic to determine if the user has won
-            const isComputerWin = // Add your logic to determine if the computer has won
-      
-            if (isUserWin || isComputerWin) {
-            endGame(isUserWin ? 'user' : 'computer');
-            } else {
-            // Switch turns between user and computer
-            currentPlayer = 'user';
-      
-            // Update turn display
-            turnDisplay.innerHTML = 'Your Turn';
-            }
+
+      gamesBoardContainer.append(gameBoardContainer)
+}
+createBoard('yellow', 'player')
+createBoard('pink', 'computer')
+
+// Creating Ships
+class ship {
+      constructor(name, length) {
+            this.name = name
+            this.length = length
       }
-      
-      // Function to end the game and declare the winner
-      function endGame(winner) {
-            isGameOver = true;
-            turnDisplay.innerHTML = '';
-            infoDisplay.innerHTML = `Game Over! The ${winner === 'user' ? 'User' : 'Computer'} wins!`;
-            // Additional actions to perform when the game ends
+}
+
+const destroyer = new ship('destroyer', 2)
+const submarine = new ship('submarine', 3)
+const cruiser = new ship('cruiser', 3)
+const battleship = new ship('battleship', 4)
+const carrier = new ship('carrier', 5)
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier]
+
+function addshipPlace(ship) {
+      const allBoardBlocks = Array.from(document.querySelectorAll('#computer div'));
+      let randomBoolean = Math.random() < 0.5; // horizontal or vertical
+      let randomStartIndex;
+  
+      do {
+          randomStartIndex = Math.floor(Math.random() * width * width); // random starting position
+      } while (!isValidPosition(ship, randomStartIndex, randomBoolean, allBoardBlocks));
+  
+      for(let i = 0; i < ship.length; i++) {
+          if(randomBoolean) { // if horizontal
+              allBoardBlocks[randomStartIndex + i].classList.add(ship.name);
+          } else { // if vertical
+              allBoardBlocks[randomStartIndex + i * width].classList.add(ship.name);
+          }
       }
-      
-      // Restart button event listener
-      restartButton.addEventListener('click', ()) => {
-            // Reset game state and board
-            isGameOver = false;
-            currentPlayer = 'user';
-            allShipsPlaced = false;
-            userSquares.forEach((square) => square.classList.remove('taken', 'horizontal', 'vertical', 'start', 'hit', 'miss'));
-            computerSquares.forEach((square) => square.classList.remove('taken', 'horizontal'));
+  }
+  
+  function isValidPosition(ship, startIndex, isHorizontal, boardBlocks) {
+      for(let i = 0; i < ship.length; i++) {
+          if (isHorizontal) {
+              if (startIndex % width < width - ship.length + 1 && boardBlocks[startIndex + i].className === "block") {
+                  continue;
+              } else {
+                  return false;
+              }
+          } else { // vertical
+              if (startIndex < width * (width - ship.length + 1) && boardBlocks[startIndex + i * width].className === "block") {
+                  continue;
+              } else {
+                  return false;
+              }
+          }
       }
+      return true;
+  }
+  
+  ships.forEach(ship => addshipPlace(ship));
+  
+
+
+// Drag and Drop
+  const playerBoard = document.querySelector("#player");
+
+// Add event listeners to the ships
+const draggableShips = document.querySelectorAll("[draggable='true']");
+draggableShips.forEach(ship => {
+    ship.addEventListener("dragstart", dragStart);
+});
+
+// Add event listeners to the player's board
+playerBoard.addEventListener("dragover", dragOver);
+playerBoard.addEventListener("drop", drop);
+
+// Define drag event handlers
+function dragStart(e) {
+    e.dataTransfer.setData("text/plain", e.target.dataset.ship);
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+      e.preventDefault();
+      const shipName = e.dataTransfer.getData("text/plain");
+      const ship = ships.find(s => s.name === shipName);
+  
+      if (ship) {
+          const blockIndex = parseInt(e.target.id, 10);
+          if (isValidPosition(ship, blockIndex, isHorizontal, Array.from(playerBoard.children))) {
+              for (let i = 0; i < ship.length; i++) {
+                  const newBlock = document.createElement("div");
+                  newBlock.classList.add("block", ship.name);
+                  if (isHorizontal) {
+                      newBlock.style.width = "20px";
+                      newBlock.style.height = "10px";
+                      playerBoard.children[blockIndex + i].replaceWith(newBlock);
+                  } else {
+                      newBlock.style.width = "10px";
+                      newBlock.style.height = "20px";
+                      playerBoard.children[blockIndex + i * width].replaceWith(newBlock);
+                  }
+              }
+  
+              // Remove the ship from the options container
+              const shipToRemove = document.querySelector(`.${shipName}-preview`);
+              if (shipToRemove) {
+                  shipToRemove.remove();
+              }
+          }
+      }
+  }
+  
+  
+  
+  
